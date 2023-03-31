cd25ad7007d9c1f3253ebfaba03ff1ee61daf7e2
sadegh <azarkaman.net@gmail.com>
2023-03-31 16:28:39 +0330

    add tests

--- src/Infrastructure/Persistence/Configurations/SalaryItemsConfiguration.cs
+++ src/Infrastructure/Persistence/Configurations/SalaryItemsConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using Entekhab.Salary.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Entekhab.Salary.Infrastructure.Persistence.Configurations;
 
@@ -9,5 +10,6 @@ public class SalaryItemsConfiguration: IEntityTypeConfiguration<SalaryData>
     public void Configure(EntityTypeBuilder<SalaryData> builder)
     {
         builder.HasKey(e => e.Id);
+        builder.Property(r => r.OverTimeCalculatorType).HasConversion(new EnumToStringConverter<CalculatorType>());
     }
 }
