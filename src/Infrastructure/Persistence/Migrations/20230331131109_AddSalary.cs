eb7f864bd61cd2c85d12541547d1363d4ab12126
sadegh <azarkaman.net@gmail.com>
2023-03-31 16:42:53 +0330

    add tests

---
+++ src/Infrastructure/Persistence/Migrations/20230331131109_AddSalary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entekhab.Salary.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSalary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SalaryData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonId = table.Column<int>(type: "int", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BasicSalary = table.Column<int>(type: "int", nullable: false),
+                    Allowance = table.Column<int>(type: "int", nullable: false),
+                    Transportation = table.Column<int>(type: "int", nullable: false),
+                    FinalSalary = table.Column<int>(type: "int", nullable: false),
+                    TaxPercent = table.Column<double>(type: "float", nullable: false),
+                    OverTimeCalculatorType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SalaryData", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SalaryData");
+        }
+    }
+}
